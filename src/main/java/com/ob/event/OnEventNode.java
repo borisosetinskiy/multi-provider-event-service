a1c758566988c7e67c8074e5876328aeb6d74113
boris <kichikawa@smarttrader.com>
2019-04-03 16:13:21 +0300

    Made 'onEventNode' default

--- src/main/java/com/ob/event/OnEventNode.java
+++ src/main/java/com/ob/event/OnEventNode.java
@@ -5,5 +5,5 @@ package com.ob.event;
  * Created by boris on 1/29/2017.
  */
 public interface OnEventNode {
-    void onEventNode(EventNodeObject eventNode);
+    default void onEventNode(EventNodeObject eventNode){};
 }
