4f646ec0a31e80507481093ca11cab03b1b2bcab
boris <kichikawa@smarttrader.com>
2019-05-27 13:52:06 +0300

    +

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module multi.provider.event.service {
-    exports com.ob.event.akka;
-    requires akka.actor;
-    requires scala.library;
-    requires typesafe.config;
-    requires org.slf4j;
-}
